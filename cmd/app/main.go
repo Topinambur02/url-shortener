1dee0f05354c6e42ca25fd82cae8fe269bfca255
Topinambur02 <alext0408@yandex.ru>
2026-06-05 11:09:46 +0300

    #3 feat: add config from .env and update http server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/topinambur02/url-shortener/internal/config"
+	"github.com/topinambur02/url-shortener/pkg/shutdown"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
-}
-
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8080", nil)
+	cfg, err := config.LoadConfig(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	} else {
+		log.Print("Default configuration loaded successfully")
+	}
+
+	host := cfg.App.Host
+	port := strconv.Itoa(cfg.App.Port)
+	address := host + ":" + port
+
+	log.Printf("Server will listen on %s", address)
+
+	server := &http.Server{
+		Addr:              address,
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		log.Printf("Starting HTTP server on %s", address)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	log.Println("Setting up graceful shutdown")
+	shutdown.GracefulShutdown([]os.Signal{syscall.SIGABRT, syscall.SIGQUIT, syscall.SIGHUP, os.Interrupt, syscall.SIGTERM}, server.Shutdown)
+	log.Println("Server exited gracefully")
 }
 
---
+++ internal/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	App struct {
+		Host string `env:"HOST" env-default:"localhost" yaml:"host"`
+		Port int    `env:"PORT" env-default:"8080" yaml:"port"`
+	} `yaml:"app"`
+
+	DB struct {
+		Host     string `env:"DB_HOST" env-default:"localhost" yaml:"host"`
+		Port     string `env:"DB_PORT" env-default:"5432" yaml:"port"`
+		User     string `env:"DB_USER" env-default:"postgres" yaml:"user"`
+		Password string `env:"DB_PASS" env-default:"postgres" yaml:"pass"`
+		Name     string `env:"DB_NAME" env-default:"meeting_room_booking_service_db" yaml:"name"`
+	} `yaml:"db"`
+
+	DSN string
+}
+
+func (c *Config) UpdateDSN() {
+	c.DSN = fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		c.DB.Host, c.DB.User, c.DB.Password, c.DB.Name, c.DB.Port,
+	)
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	var cfg Config
+	fileInfo, err := os.Stat(filename)
+
+	if err == nil && fileInfo.Size() > 0 {
+		err = cleanenv.ReadConfig(filename, &cfg)
+
+		if err != nil {
+			if !os.IsNotExist(err) {
+				return nil, fmt.Errorf("failed to parse config file: %w", err)
+			}
+		}
+	}
+
+	err = cleanenv.ReadEnv(&cfg)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to read environment: %w", err)
+	}
+
+	cfg.UpdateDSN()
+
+	return &cfg, nil
+}
