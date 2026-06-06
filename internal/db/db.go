55776212b66b84d871213230b1bb34293fc316bf
Topinambur02 <alext0408@yandex.ru>
2026-06-06 17:36:30 +0300

    #37 feat: add max_connections for db and options for server

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 
@@ -11,11 +12,21 @@ import (
 
 func InitDB(ctx context.Context, cfg *config.Config) (*pgxpool.Pool, error) {
 	dsn := cfg.DSN
-	pool, err := pgxpool.New(ctx, dsn)
-
+	poolConfig, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
-        return nil, fmt.Errorf("unable to create connection pool: %w", err)
-    }
+		return nil, fmt.Errorf("unable to parse database config: %w", err)
+	}
+
+	poolConfig.MaxConns = 200
+	poolConfig.MinConns = 100
+
+	poolConfig.MaxConnIdleTime = time.Minute * 30
+	poolConfig.MaxConnLifetime = time.Hour
+
+	pool, err := pgxpool.NewWithConfig(ctx, poolConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create connection pool: %w", err)
+	}
 
 	if err := pool.Ping(ctx); err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
